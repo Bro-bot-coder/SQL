c8918883ed84400ca78d65f9b49392331bf23be0
Bro-bot-coder <asgarov.en@gmail.com>
2022-11-22 11:01:41 +0400

    Revert "non user id"
    This reverts commit 6406f797c388dfe8cfb7824afe02ef8662696795.

--- Olmayan ID_Nomreler.sql
+++
@@ -1,7 +0,0 @@
-with  
-   seq as (select top 1000 row_number() over(order by Badgenumber) as NonUserID
-            from dbo.USERINFO) 
-         select seq.NonUserID 
-		    from seq left outer join userinfo u 
-			  on (seq.NonUserID = u.badgenumber) 
-     where u.badgenumber is null;
